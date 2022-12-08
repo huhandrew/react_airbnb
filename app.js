5a356193674377fe8720f027e716cb89ac4ebca9
huhandrew <huhandrey13@gmail.com>
2022-12-08 13:00:06 +0500

    added airbnb logo+ styling navbar

---
+++ app.js
@@ -0,0 +1,11 @@
+import { directive } from "babel-types"
+import React from "react"
+import Navbar from "./components/navbar.js"
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
